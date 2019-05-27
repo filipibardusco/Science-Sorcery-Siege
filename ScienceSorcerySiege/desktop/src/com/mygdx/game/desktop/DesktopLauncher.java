f6722897e394714a02461ca0947c84b67e271345
Filipi Bardusco <filipi@bardusco.com>
2019-05-26 20:09:42 -0400

    Add files via upload

--- ScienceSorcerySiege/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ ScienceSorcerySiege/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,11 @@ import com.mygdx.game.ScienceSorcerySiege;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Science Sorcery Siege!";
+		config.height = 960;
+		config.width = 960;
 		new LwjglApplication(new ScienceSorcerySiege(), config);
+		
+		
 	}
 }
